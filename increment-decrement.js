768a274d06df9d790b375c8fbf2f1c48e8c85b7b
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-03-12 23:48:12 +0500

    Add files via upload
    Governor  Sindh IT Initiative Program

---
+++ increment-decrement.js
@@ -0,0 +1,82 @@
+"use strict";
+//increment/Decrement
+
+//increment
+//let x = 10;
+//x=10+1;
+
+//pre increment
+//++x;
+//console.log(x);
+
+//let a = 200
+//++a
+//console.log(++a)
+//console.log(++a)
+
+//practice
+
+//let num1=10
+//console.log((num1)+(num1)) 
+//console.log((11)+(12))   
+
+//let num1=10
+//++num1
+//console.log((++num1)+(++num1))  //23
+//             11   +   12     //23
+
+//post increment
+//let y = 100
+//y++
+//console.log(y++)      //100
+//console.log(y++)      //101
+//console.log(y++)       //102
+//console.log(y)         //103
+//console.log(y++)      //103
+//let x = 20
+//let num1=++x
+//console.log(num1)        //21
+//let x = 50
+//let num2=x++
+//console.log(num2)           //50
+//console.log(num2+5)         //55
+
+//practice
+//let num1=10
+//num1++
+//console.log((num1++)+(num1++))  //21
+//             10   +   11     //21
+
+//decrement
+//let y = 10;
+//y=10-1;
+//console.log(y)          //9
+
+//pre decrement
+//let a = 6
+//--a;
+//console.log(--a)
+
+//let y = 10
+//console.log(--y)   //9
+//console.log(--y)   //8
+
+//post decrement
+//let y = 8
+//y--;
+//console.log(y--)  //8 
+//console.log(y--)  //7
+
+//
+//let number1=20
+//console.log((++number1)+(number1++)+(number1) )
+              //   21    +      21   +     22    //64
+
+
+let number1=15
+//let variable1=number1++
+//let variable2=number1
+//let variable3=++number1
+console.log((number1++)+(number1)+(++number1) )   
+              //   15     +      16    +    17  48
+                  
---
+++ increment-decrement.ts
@@ -0,0 +1,10 @@
+//increment/Decrement
+//increment
+//let x=10;
+//x=10+1;
+//pre increment
+//++x;
+//console.log(x); 
+let a=200;
+++a;
+console.log(a); 
